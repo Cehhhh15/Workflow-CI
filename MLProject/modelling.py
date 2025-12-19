1194765807043f5cc248baa2aa699cc17c1366ec
Christian Daniel Wijaya <christian.danielwijaya@gmail.com>
2025-12-19 09:29:09 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -17,7 +17,7 @@ def train_model():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Set Experiment
-    mlflow.set_experiment("Sleep_Disorder_Basic")
+    #mlflow.set_experiment("Sleep_Disorder_Basic")
 
     # Training dengan Autolog
     print("Mulai Training Basic...")
